231fc33e2b3d64d5b0123acdecb517cc7950372d
38219002 <38219002@L829WD210019.edu.cbe.ab.ca>
2025-09-08 11:00:50 -0600

    finished

--- OO2-Exercises/ex001/UnitCircle.java
+++ OO2-Exercises/ex001/UnitCircle.java
@@ -1,4 +1,28 @@
 
-public class UnitCircle {
+public class UnitCircle implements Comparable{
+
+    private int radius;
+    
+    public UnitCircle(int radius) {
+        this.radius = radius;
+    }
+    
+    public int getRadius() {
+        return radius;
+    }
+    @Override
+    public int compareTo(Object o) {
+        UnitCircle obj = (UnitCircle) o;
+        if (radius < obj.getRadius()) {
+            return -1;
+        } else if (radius > obj.getRadius()) {
+            return 1;
+        }
+        return 0;
+    }
+    @Override
+    public String toString() {
+        return String.format("UnitCircle[r=%d]", radius);
+    }
 
 }
