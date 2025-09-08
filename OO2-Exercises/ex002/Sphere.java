195938887cfb42937d3a6d232a0836c0d880572f
38219002 <38219002@L829WD210019.edu.cbe.ab.ca>
2025-09-08 11:14:54 -0600

    Sphere done

--- OO2-Exercises/ex002/Sphere.java
+++ OO2-Exercises/ex002/Sphere.java
@@ -1,4 +1,20 @@
 
-public class Sphere {
+public class Sphere implements GeometricSolid{
+    
+    private double radius;
+    
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getVolume() {
+        return (4.0/3.0) * (Math.PI * Math.pow(radius, 3));
+    }
+
+    @Override
+    public double getSurfaceArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
 
 }
